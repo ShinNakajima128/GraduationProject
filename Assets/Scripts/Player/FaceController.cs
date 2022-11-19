85351bfe59cffc870cd7a0102863d776c7197960
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-11-19 17:14:23 +0900

    [update]アリスの表情の変更を行うFaceControllerを修正

--- Assets/Scripts/Player/FaceController.cs
+++ Assets/Scripts/Player/FaceController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum EyeTypes : int
+public enum EyeTypes
 {
     Default, /// デフォルト.
     Angry, /// 怒ってる.
@@ -20,34 +20,46 @@ public class FaceController : MonoBehaviour
 {
     #region serialize
     [SerializeField]
+    EyeTypes _TestEyeType = default;
+
+    [SerializeField]
     Renderer _eyeRenderer = default;
     #endregion
     #region private
     Dictionary<EyeTypes, Vector2> _eyeTypeDic = new Dictionary<EyeTypes, Vector2>();
     Material _eyeMat;
+    bool _init = false;
     #endregion
     #region property
     #endregion
 
+    private void OnValidate()
+    {
+        if (_init)
+        {
+            ChangeEyeType(_TestEyeType);
+        }
+    }
     private void Awake()
     {
         int count = 0;
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < 4; i++)
         {
-            for (int n = 0; n < 4; n++)
+            for (int n = 0; n < 2; n++)
             {
-                _eyeTypeDic.Add((EyeTypes)count, new Vector2(i, (n + 1) * 0.25f));
-                Debug.Log($"offset{(EyeTypes)count} x:{i + n}, y { new Vector2(i, (n + 1) * 0.25f)}");
+                _eyeTypeDic.Add((EyeTypes)count, new Vector2(n * 0.5f, i * - 0.25f));
+                //Debug.Log($"offset{(EyeTypes)count} x:{n * 0.5f}, y { i * -0.25f}");
                 count++;
             }
         }
-        _eyeMat = _eyeRenderer.material;
+        _eyeMat = _eyeRenderer.materials[1];
+        _init = true;
     }
 
     public void ChangeEyeType(EyeTypes type)
     {
-        _eyeMat.SetTextureOffset("_BaseMap", GetEyeOffset(type));
-        _eyeRenderer.material = _eyeMat;
+        _eyeMat.SetTextureOffset("_MainTex", GetEyeOffset(type));
+        _eyeRenderer.materials[1] = _eyeMat;
     }
 
     Vector2 GetEyeOffset(EyeTypes type)
