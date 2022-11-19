8ab0e72f1a879caa11c3c06cb55a152e4d7d7953
SakamakiRyu <potesara031w@gmail.com>
2022-11-19 22:13:06 +0900

    更新のコミット

--- Assets/Scripts/Stages/NewStage2/Stage2Mugcup.cs
+++ Assets/Scripts/Stages/NewStage2/Stage2Mugcup.cs
@@ -6,6 +6,6 @@ public class Stage2Mugcup : MonoBehaviour
 {
     public void MoveRequest(Vector3 position, float duration, Action action = null)
     {
-        transform.DOLocalMove(position, duration).OnComplete(() => action());
+        transform.DOLocalMove(position, duration).OnComplete(() => action?.Invoke());
     }
 }
