6b67697994f7eef7463c7189710c1e30224128d8
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-11-29 02:19:46 +0900

    [update]ミニゲームをクリアしたかどうかでロビーに戻っていた時の処理を変更する処理を追加

--- Assets/Scripts/Stages/NewStage2/Stage2GameManager.cs
+++ Assets/Scripts/Stages/NewStage2/Stage2GameManager.cs
@@ -85,6 +85,7 @@ public class Stage2GameManager : MonoBehaviour
                     if (IsClear)
                     {
                         // ƒƒr[‚ÉˆÚ“®
+                        GameManager.SaveStageResult(true);
                         TransitionManager.SceneTransition(SceneType.Lobby);
                     }
                     break;
