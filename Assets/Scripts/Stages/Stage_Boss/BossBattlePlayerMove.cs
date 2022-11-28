886898051153eb2743f64560a06408afa391cbf9
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-11-29 05:49:17 +0900

    [add]BossSceneを追加し、カメラ演出後、プレイヤーが移動する場面まで仮作成

---
+++ Assets/Scripts/Stages/Stage_Boss/BossBattlePlayerMove.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBattlePlayerMove : MonoBehaviour, IMovable
+{
+    #region serialize
+    [SerializeField]
+    float _moveSpeed = 5.0f;
+
+    [SerializeField]
+    float _turnSpeed = 5.0f;
+    #endregion
+
+    #region private
+    Rigidbody _rb;
+    Animator _anim;
+    Vector3 _dir;
+    bool _isMoving;
+    #endregion
+
+    private void Awake()
+    {
+        TryGetComponent(out _rb);
+        TryGetComponent(out _anim);
+    }
+
+    private void Start()
+    {
+        PlayerMovable(true);
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isMoving)
+        {
+            if (_dir == Vector3.zero)
+            {
+                _rb.velocity = new Vector3(0f, _rb.velocity.y, 0f);
+            }
+            else
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(_dir);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.fixedDeltaTime * _turnSpeed);
+                Vector3 velocity = _dir.normalized * _moveSpeed;
+                velocity.y = _rb.velocity.y;
+                _rb.velocity = velocity;
+            }
+
+            CharacterAnimation();
+        }
+    }
+    public void SetDirection(Vector3 dir)
+    {
+        _dir = Vector3.forward * dir.y + Vector3.right * dir.x;
+        _dir = Camera.main.transform.TransformDirection(_dir);    // メインカメラを基準に入力方向のベクトルを変換する
+        _dir.y = 0;  // y 軸方向はゼロにして水平方向のベクトルにする
+    }
+    void CharacterAnimation()
+    {
+        if (_anim)
+        {
+            Vector3 velo = _rb.velocity;
+            velo.y = 0;
+            _anim.SetFloat("Move", velo.magnitude);
+        }
+    }
+
+    void PlayerMovable(bool isMove)
+    {
+        _isMoving = isMove;
+    }
+}
---
+++ Assets/Scripts/Stages/Stage_Boss/BossStageManager.cs
@@ -0,0 +1,174 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+/// <summary>
+/// ボスステージの機能を管理するマネージャー
+/// </summary>
+public class BossStageManager : StageGame<BossStageManager>
+{
+    #region serialize
+    [Header("Variables")]
+    [Tooltip("戦闘回数")]
+    [SerializeField]
+    int _battleNum = 3;
+
+    [Tooltip("別のカメラへ遷移する際にかける時間")]
+    [SerializeField]
+    float _cameraBlendTime = 2.0f;
+
+    [SerializeField]
+    float _closeupTime = 0.25f;
+
+    [Header("Cameras")]
+    [Tooltip("Cinemachineを管理するBrain")]
+    [SerializeField]
+    CinemachineBrain _cameraBrain = default;
+
+    [Tooltip("女王に注目するカメラ")]
+    [SerializeField]
+    CinemachineVirtualCamera _directionCamera = default;
+
+    [Tooltip("女王に更にズームインするカメラ")]
+    [SerializeField]
+    CinemachineVirtualCamera _closeupCamera = default;
+
+    [Tooltip("戦闘中の見下ろしカメラ")]
+    [SerializeField]
+    CinemachineVirtualCamera _battleCamera = default;
+
+    [Header("Component")]
+    [SerializeField]
+    MessagePlayer _messagePlayer = default;
+    #endregion
+    #region private
+    /// <summary> 演出中かどうか </summary>
+    bool _isDirecting = false;
+    #endregion
+    #region public
+    public override event Action GameSetUp;
+    public override event Action GameStart;
+    public override event Action GamePause;
+    public override event Action GameEnd;
+    #endregion
+    #region property
+    #endregion
+
+    protected override void Start()
+    {
+        base.Start();
+        Init();
+        OnGameStart();
+    }
+
+    public override void OnGameSetUp()
+    {
+        GameSetUp?.Invoke();
+    }
+
+    public override void OnGameStart()
+    {
+        GameStart?.Invoke();
+        StartCoroutine(GameStartCoroutine());
+    }
+
+    public override void OnGameEnd()
+    {
+        GameEnd?.Invoke();
+    }
+
+    protected override IEnumerator GameStartCoroutine(Action action = null)
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        //カメラをボスに寄せる
+        CameraBlend(CameraType.Direction, _cameraBlendTime);
+
+        yield return new WaitForSeconds(_cameraBlendTime);
+
+        //ボス戦開始時の演出を再生
+        StartCoroutine(_messagePlayer.PlayMessageCorountine(MessageType.Stage_Boss_Start, () =>
+        {
+            _isDirecting = true;
+        }));
+
+        yield return new WaitUntil(() => _isDirecting);
+
+        _isDirecting = false;
+
+        CameraBlend(CameraType.Battle, _cameraBlendTime);
+
+        yield return new WaitForSeconds(_cameraBlendTime);
+
+        StartCoroutine(InGameCoroutine());
+    }
+
+    protected override IEnumerator GameEndCoroutine(Action action = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void Init()
+    {
+        _messagePlayer.Closeup += OnCloseup;
+        _messagePlayer.Reset += OnReset;
+    }
+
+    IEnumerator InGameCoroutine()
+    {
+        yield return null;
+    }
+
+    /// <summary>
+    /// 現在のカメラから別のカメラへ遷移する
+    /// </summary>
+    /// <param name="type"> カメラの種類 </param>
+    /// <param name="blendTime"> 遷移にかける時間 </param>
+    void CameraBlend(CameraType type, float blendTime)
+    {
+        _cameraBrain.m_DefaultBlend.m_Time = blendTime;
+
+        switch (type)
+        {
+            case CameraType.Default:
+                _directionCamera.Priority = 9;
+                _closeupCamera.Priority = 9;
+                _battleCamera.Priority = 9;
+                break;
+            case CameraType.Direction:
+                _directionCamera.Priority = 15;
+                _closeupCamera.Priority = 9;
+                _battleCamera.Priority = 9;
+                break;
+            case CameraType.Battle:
+                _directionCamera.Priority = 9;
+                _closeupCamera.Priority = 9;
+                _battleCamera.Priority = 15;
+                break;
+            case CameraType.Direction_Closeup:
+                _directionCamera.Priority = 9;
+                _closeupCamera.Priority = 15;
+                _battleCamera.Priority = 9;
+                break;
+            default:
+                break;
+        }
+    }
+    void OnCloseup()
+    {
+        CameraBlend(CameraType.Direction_Closeup, _closeupTime);
+    }
+    void OnReset()
+    {
+        CameraBlend(CameraType.Direction_Closeup, _cameraBlendTime);
+    }
+}
+public enum CameraType
+{
+    Default,
+    Direction,
+    Battle,
+    Direction_Closeup
+}
