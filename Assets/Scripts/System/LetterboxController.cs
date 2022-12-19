5de9b7c3b4f93a705e055276dedcd73954c8e7ca
ShinNakajima128 <ukauka224@i.softbank.jp>
2022-12-20 07:32:06 +0900

    [add]演出中の画面上下の帯(レターボックス)を追加

---
+++ Assets/Scripts/System/LetterboxController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class LetterboxController : MonoBehaviour
+{
+    #region serialize
+    [SerializeField]
+    Transform[] _letterbox = default;
+    #endregion
+
+    #region private
+    #endregion
+
+    #region public
+    #endregion
+    #region property
+    public static LetterboxController Instance { get; private set; }
+    #endregion
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        ActivateLetterbox(false, 0);
+    }
+
+    public static void ActivateLetterbox(bool value, float time = 1.0f)
+    {
+        if (value)
+        {
+            Instance._letterbox[0].DOLocalMoveY(500, time);
+            Instance._letterbox[1].DOLocalMoveY(-500, time);
+        }
+        else
+        {
+            Instance._letterbox[0].DOLocalMoveY(600, time);
+            Instance._letterbox[1].DOLocalMoveY(-600, time);
+        }
+    }
+}
