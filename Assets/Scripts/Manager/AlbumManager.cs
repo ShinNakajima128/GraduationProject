f348175b530f2fdf1801101b484e459ff4f8c1db
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-01-02 21:04:17 +0900

    [update]ロビーのアルバム機能を作成中

--- Assets/Scripts/Manager/AlbumManager.cs
+++ Assets/Scripts/Manager/AlbumManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
 
 public class AlbumManager : MonoBehaviour
 {
@@ -9,9 +10,14 @@ public class AlbumManager : MonoBehaviour
     [Header("UI")]
     [SerializeField]
     CanvasGroup _albumPanelGroup = default;
+
+    [SerializeField]
+    Transform _albumTrans = default;
     #endregion
 
     #region private
+    bool _isCanOpened = false;
+    bool _isOpened = false;
     #endregion
 
     #region public
@@ -24,5 +30,38 @@ public class AlbumManager : MonoBehaviour
     private void Awake()
     {
         Instance = this;
+    }
+
+    public void Start()
+    {
+        LobbyManager.Instance.PlayerMove += ActivatePanel;
+    }
+
+    /// <summary>
+    /// アルバムのパネルの表示/非表示を切り替える
+    /// </summary>
+    /// <param name="isOpened"> 表示するか非表示にするかのフラグ </param>
+    void ActivatePanel(bool isCanOpened)
+    {
+        _isCanOpened = isCanOpened;
+
+        if (_isOpened)
+        {
+            _albumPanelGroup.alpha = 1;
+        }
+        else
+        {
+            _albumPanelGroup.alpha = 0;
+        }
+    }
+
+    void ActiveAlbum(bool isOpened)
+    {
+        _isOpened = isOpened;
+
+        if (_isOpened)
+        {
+
+        }
     }
 }
