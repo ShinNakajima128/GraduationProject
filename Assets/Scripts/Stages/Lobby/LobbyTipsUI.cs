6b65a763daa2ede3d2af5979f9206b897fd0d1be
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-01-18 12:56:29 +0900

    [update]ロビーUIの機能を仮作成

---
+++ Assets/Scripts/Stages/Lobby/LobbyTipsUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ロビーでのヒントを表示する機能を持つコンポーネント
+/// </summary>
+public class LobbyTipsUI : MonoBehaviour
+{
+    #region serialize
+    #endregion
+
+    #region private
+    #endregion
+
+    #region public
+    #endregion
+
+    #region property
+    #endregion
+}
