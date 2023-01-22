113472dd9ccbd905c535eb6bcdd634569577b958
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-01-22 20:43:18 +0900

    [update]ロビーのモデルの差し替えと、ポーズ画面の表示切替の入力処理を追加

--- Assets/Scripts/UI/ButtonReactionController.cs
+++ Assets/Scripts/UI/ButtonReactionController.cs
@@ -13,14 +13,20 @@ public class ButtonReactionController : MonoBehaviour
     [SerializeField]
     float _animScale = 1.3f;
 
+    [Tooltip("カーソルのX座標の補正値")]
+    [SerializeField]
+    float _correctionValue_x = 2.5f;
+
+    [Tooltip("カーソルのY座標の補正値")]
+    [SerializeField]
+    float _correctionValue_y = 2.5f;
+
     [SerializeField]
     float _animTime = 0.25f;
     #endregion
 
     #region private
     EventTrigger _trigger;
-
-    Transform _cursorImageTrans = default;
     #endregion
     #region public
     #endregion
@@ -47,7 +53,10 @@ public class ButtonReactionController : MonoBehaviour
 
     void OnSelectEvent()
     {
-        ButtonCursor.MoveCursor(new Vector3(transform.position.x + 2.5f, transform.position.y + 2.5f, transform.position.z), transform);
+        ButtonCursor.MoveCursor(new Vector3(transform.position.x + _correctionValue_x, 
+                                            transform.position.y + _correctionValue_y, 
+                                            transform.position.z), 
+                                            transform);
         transform.DOScale(_animScale, _animTime);
     }
     void OnDeselectEvent()
