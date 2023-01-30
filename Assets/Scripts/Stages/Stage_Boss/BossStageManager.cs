1a44678b853bcf019ec43a5f9403acdfc4b42816
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-01-31 01:05:35 +0900

    [update]ボス戦の当たり判定を調整

--- Assets/Scripts/Stages/Stage_Boss/BossStageManager.cs
+++ Assets/Scripts/Stages/Stage_Boss/BossStageManager.cs
@@ -411,7 +411,7 @@ public class BossStageManager : StageGame<BossStageManager>
     {
         CameraBlend(CameraType.FinishBattle, 2.0f);
 
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(4.0f);
 
         _infoImages[1].enabled = true;
         AudioManager.PlayBGM(BGMType.BossStage_Clear, false);
