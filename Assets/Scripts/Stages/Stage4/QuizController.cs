bfe94a28d19851bb63caecbecd561b91947b86b7
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-01-31 19:38:53 +0900

    [update]ステージ4のクイズパート時にトランプ兵が映り込む問題を修正

--- Assets/Scripts/Stages/Stage4/QuizController.cs
+++ Assets/Scripts/Stages/Stage4/QuizController.cs
@@ -137,13 +137,14 @@ public class QuizController : MonoBehaviour
                 break;
         }
 
+        Debug.Log($"åªç›ÇÃâÒìöêîÅF{_currentAnswerValue}");
         ChoiceButtonSetUp(type);
 
         yield return null;
 
         _playerChoiceValue = _choiceButtonList[0].ChoiceAnswerValue;
         _currentButtonIndex = _choiceButtonList[0].ButtonIndex;
-
+        manager.TrumpGenerator.Return();
         if (!_debugMode)
         {
             while (timer < _answerTime && !_isChoiced)
